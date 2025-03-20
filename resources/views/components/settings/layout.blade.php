051329979b5575f6e286d3bd4a995259b3b6dfdd
rowinf <964049+rowinf@users.noreply.github.com>
2025-03-20 14:41:43 +1300

    settings page nav icons

--- resources/views/components/settings/layout.blade.php
+++ resources/views/components/settings/layout.blade.php
@@ -1,13 +1,13 @@
 <div class="flex items-start max-md:flex-col">
     <div class="mr-10 pt-5 pl-8 pr-4 border-r dark:border-zinc-600">
         <flux:navlist class="h-screen gap-1 w-52">
-            <flux:navlist.item href="{{ route('settings.appearance') }}" icon="sun" wire:navigate>{{ __('Color Theme') }}</flux:navlist.item>
-            <flux:navlist.item href="{{ route('settings.font-theme') }}" icon="bold" wire:navigate>{{ __('Font Theme') }}</flux:navlist.item>
-            <flux:navlist.item href="{{ route('settings.password') }}" icon="lock-closed" wire:navigate>{{ __('Change Password') }}</flux:navlist.item>
+            <flux:navlist.item href="{{ route('settings.appearance') }}" icon="icon-sun" wire:navigate>{{ __('Color Theme') }}</flux:navlist.item>
+            <flux:navlist.item href="{{ route('settings.font-theme') }}" icon="icon-font" wire:navigate>{{ __('Font Theme') }}</flux:navlist.item>
+            <flux:navlist.item href="{{ route('settings.password') }}" icon="icon-lock" wire:navigate>{{ __('Change Password') }}</flux:navlist.item>
             <flux:separator />
             <form method="POST" action="{{route('logout')}}">
                 @csrf
-                <flux:navlist.item type="submit" icon="arrow-left-start-on-rectangle">{{ __('Logout') }}</flux:navlist.item>
+                <flux:navlist.item type="submit" icon="icon-logout">{{ __('Logout') }}</flux:navlist.item>
             </form>
         </flux:navlist>
     </div>
