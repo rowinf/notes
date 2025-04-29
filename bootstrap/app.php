f919bed85325ce9f7b99219fb09eb3cc11315972
rowinf <964049+rowinf@users.noreply.github.com>
2025-04-29 16:09:27 +1200

    removed trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(TrustProxies::class);
-    })
+    // ->withMiddleware(function (Middleware $middleware) {
+    //     $middleware->append(TrustProxies::class);
+    // })
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
